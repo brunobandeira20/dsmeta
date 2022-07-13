a86a6a10fa02e46179059a9e5a26bc6791a5e20e
brunobandeira20 <brunobandeira20@gmail.com>
2022-07-12 23:59:44 -0300

    useStates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
-import SalesCard from "./components/SalesCard"
-import Header from "./components/Header"
+import SalesCard from "./components/SalesCard";
+import Header from "./components/Header";
 
 function App() {
-  return(
+  return (
     <>
-      <Header/>
+      <Header />
       <main>
         <section id="sales">
           <div className="dsmeta-container">
-            <SalesCard/>
+            <SalesCard />
           </div>
         </section>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,19 +1,22 @@
-import logo from '../../assets/img/logo.svg'
-import './styles.css'
+import logo from "../../assets/img/logo.svg";
+import "./styles.css";
 
 function Header() {
-  return(
-      <header>
-          <div className="dsmeta-logo-container">
-              <img src={logo} alt="DSMeta" />
-              <h1>DSMeta</h1>
-              <p>
-                Desenvolvido por
-                <a href="https://www.instagram.com/brunobandeira20"> @brunobandeira</a>
-              </p>
-          </div>
-      </header>
-  )
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://www.instagram.com/brunobandeira20">
+            {" "}
+            @brunobandeira
+          </a>
+        </p>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
